dab715a61f916547df1ff4ea2d50ae7283741c27
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-16 00:28:47 +1000

    Define basic structure and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="editor-wrapper">
+        <h1>Markdown Editor</h1>
+        <textarea name="" id="editor" cols="30" rows="10" autoFocus={true} placeholder="Please enter something here..."></textarea>
+      </div>
+      <div className="preview-wrapper">
+        <h1>Markdown Previwer</h1>
+        <div id="preview"></div>
+      </div>
     </div>
   );
 }
