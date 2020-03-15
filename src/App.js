1c75866ca07d5cf251528a258b0587b78f73b2e9
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-16 00:42:07 +1000

    Define function for read text and display on Preview

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 
 function App() {
+
+  const [text, setText] = useState("");
+
+
   return (
     <div className="App">
       <div className="editor-wrapper">
         <h1>Markdown Editor</h1>
-        <textarea name="" id="editor" cols="30" rows="10" autoFocus={true} placeholder="Please enter something here..."></textarea>
+        <textarea name="" id="editor" cols="30" rows="10" autoFocus={true} placeholder="Please enter something here..." onChange={event => setText(event.target.value)}></textarea>
       </div>
       <div className="preview-wrapper">
         <h1>Markdown Previwer</h1>
-        <div id="preview"></div>
+        <div id="preview">{text}</div>
       </div>
     </div>
   );
